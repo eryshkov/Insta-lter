0651c9f1c5d101616d3fda038ae08ef9c5fd1b53
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-12-14 11:57:58 +0300

    outlets and actions added

--- Instaﬁlter/ViewController.swift
+++ Instaﬁlter/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func changeFilter(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+        
+    }
 }
 
